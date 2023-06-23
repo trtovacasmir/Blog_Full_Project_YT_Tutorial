2b63d0cef020df221359371455cf1b3d220d3534
trtovacasmir <trtovacasmir@gmail.com>
2023-06-23 17:18:25 +0200

    Creating posts

--- admin/add-post.php
+++ admin/add-post.php
@@ -1,6 +1,15 @@
 <?php
-include 'partials/header.php'
+include 'partials/header.php';
+
+// fetch categories from database
+$query = "SELECT * FROM categories";
+$categories = mysqli_query($connection, $query);
 ?>
+
+
+
+
+
 <section class="form__section">
     <div class="container form__section-container">
         <h2>Add Post</h2>
@@ -8,26 +17,26 @@ include 'partials/header.php'
             <p>This is an error message</p>
         </div>
 
-        <form action="" enctype="multipart/form-data">
-            <input type="text" placeholder="Title">
-            <select>
-                <option value="1">Travel</option>
-                <option value="1">Art</option>
-                <option value="1">Science & Technology</option>
-                <option value="1">Travel</option>
-                <option value="1">Travel</option>
-                <option value="1">Travel</option>
+        <form action="<?= ROOT_URL ?>admin/add-post-logic.php" enctype="multipart/form-data" method="POST">
+            <input type="text" name="title" placeholder="Title">
+            <select name="category">
+                <?php while ($category = mysqli_fetch_assoc($categories)) : ?>
+                <option value="<?= $category['id'] ?>"><?= $category['title'] ?></option>
+                <?php endwhile ?>
+               
             </select>
-            <textarea rows="10" placeholder="Body"></textarea>
+            <textarea rows="10" name="body" placeholder="Body"></textarea>
+            <?php if(isset($_SESSION['user_is_admin'])): ?>
             <div class="form__control inline">
-                <input type="checkbox"id="is_featured">
+                <input type="checkbox" name="is_featured" value="1" id="is_featured" checked>
                 <label for="is_featured" checked>Featured</label>
             </div>
+            <?php endif ?>
             <div class="form__control">
                 <label for="thumbnail">Add Thumbnail</label>
-                <input type="file" id="thumbnail">
+                <input type="file" name="thumbnail" id="thumbnail">
             </div>
-            <button type="submit" class="btn">Add Post</button>
+            <button type="submit" name="submit" class="btn">Add Post</button>
         </form>
 
     </div>
